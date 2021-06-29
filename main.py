c144ba20e88ee8805baba8c56a7ed16bc4521291
yadira-ferrer <yyfm.95@gmail.com>
2021-06-28 19:29:47 -0600

    Actualización main.py
    Se añaden endpoints para añadir producto al carrito de compras, obtener producto por id e insertar producto en la entidad.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, json, request, jsonify
+from flask.wrappers import Response
 from flask_mysqldb import MySQL
+
+import decimal
 
 app = Flask(__name__)
 
@@ -10,6 +13,9 @@ app.config['MYSQL_DB'] = 'mydb'
 
 mysql = MySQL(app)
 
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal): return float(obj)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -77,6 +83,72 @@ def getproductos():
     rows = cur.fetchall()
     return jsonify(rows[0])
 
+# Insertar un producto
+@app.route('/producto/1', methods=['POST'])
+def addProduct():
+    try:
+        cursor = mysql.connection.cursor()
+        name = request.json['nombre']
+        description = request.json['descripcion']
+        price = request.json['precio']
+        weight = request.json['peso']
+        stock = request.json['stock']
+        category = request.json['id_categoria']
+        image = request.json['nombre_img']
+
+        query = '''INSERT INTO producto (nombre, descripcion, precio, peso, stock, categoria_idcategoria, nombreImagen)
+                VALUES(%s,%s,%s,%s,%s,%s,%s)'''
+        values = (name,description,float(price),float(weight),int(stock),int(category),image)
+
+        cursor.execute(query, values)
+        cursor.connection.commit()
+        return Response('{"msg":"Producto insertado exitosamente."}', status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"Error al insertar producto."}', status=400, mimetype='application/json')
+    finally:
+        cursor.close()
+
+# Insertar un producto al carrito de compras
+@app.route('/carrito/1', methods=['POST'])
+def addProductShoppingCar():
+    try:
+        cursor = mysql.connection.cursor()
+        iduser = request.json['id_usuario']
+        idproduct = request.json['id_producto']
+
+        query = '''INSERT INTO carrito (producto_idproducto, usuario_idusuario)
+                VALUES(%s,%s)'''
+        values = (int(idproduct), int(iduser))
+
+        cursor.execute(query, values)
+        cursor.connection.commit()
+        return Response('{"msg":"Producto añadido a carrito."}', status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"Error al añadir producto al carrito."}', status=400, mimetype='application/json')
+    finally:
+        cursor.close()
+
+# Obtener producto por ID
+@app.route('/producto', methods=['GET'])
+def getProductById():
+    try:
+        cursor = mysql.connection.cursor()
+        idproduct = request.json['id_producto']
+
+        query = '''SELECT * FROM producto WHERE idproducto = %s'''
+
+        cursor.execute(query, [idproduct])
+        result = cursor.fetchall()
+
+        resjson = json.dumps(result[0], cls = Encoder)
+        return Response(resjson, status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"Producto no encontrado."}', status=404, mimetype='application/json')
+    finally:
+        cursor.close()
 
 if __name__ == '__main__':
     print("iniciando ... ")
