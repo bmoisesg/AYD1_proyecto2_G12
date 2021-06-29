53bdb4733ca21df942d53ebd42f633fb286878f3
yadira-ferrer <yyfm.95@gmail.com>
2021-06-28 22:22:55 -0600

    Update main.py
    Se corrigen nombres de los endpoints y se añaden para eliminar producto del carrito, obtener lista de métodos de pago, login del usuario.

--- main.py
+++ main.py
@@ -78,15 +78,15 @@ def getnumpage():
     return jsonify(rows[0])
 
 
-@app.route('/producto',methods=['POST'])
+@app.route('/productos',methods=['POST'])
 def getproductos():
     cur = mysql.connection.cursor()
     cur.execute('''select (count(*) div 10)+1  from producto''')
     rows = cur.fetchall()
     return jsonify(rows[0])
 
-# Insertar un producto
-@app.route('/producto/1', methods=['POST'])
+# Insertar un producto en la entidad 
+@app.route('/producto/nuevo', methods=['POST'])
 def addProduct():
     try:
         cursor = mysql.connection.cursor()
@@ -112,16 +112,16 @@ def addProduct():
         cursor.close()
 
 # Insertar un producto al carrito de compras
-@app.route('/carrito/1', methods=['POST'])
+@app.route('/carrito/nuevo', methods=['POST'])
 def addProductShoppingCar():
     try:
         cursor = mysql.connection.cursor()
         iduser = request.json['id_usuario']
         idproduct = request.json['id_producto']
 
-        query = '''INSERT INTO carrito (producto_idproducto, usuario_idusuario)
-                VALUES(%s,%s)'''
-        values = (int(idproduct), int(iduser))
+        query = '''INSERT INTO carrito (producto_idproducto, usuario_idusuario, cantidad)
+                VALUES(%s,%s,%s)'''
+        values = (int(idproduct), int(iduser), 1)
 
         cursor.execute(query, values)
         cursor.connection.commit()
@@ -133,7 +133,7 @@ def addProductShoppingCar():
         cursor.close()
 
 # Obtener producto por ID
-@app.route('/producto', methods=['GET'])
+@app.route('/producto', methods=['POST'])
 def getProductById():
     try:
         cursor = mysql.connection.cursor()
@@ -149,6 +149,92 @@ def getProductById():
     except Exception as e:
         print('[ERROR]:',e)
         return Response('{"msg":"Producto no encontrado."}', status=404, mimetype='application/json')
+    finally:
+        cursor.close()
+
+# Obtener todos los tipos de métodos de pago
+@app.route('/metodoPago', methods=['GET'])
+def getPaymentMethod():
+    try:
+        cursor = mysql.connection.cursor()
+
+        query = '''SELECT * FROM metodoPago;'''
+
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        resjson = json.dumps(result, cls = Encoder)
+        return Response(resjson, status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"Error al obtener metodos de pago."}', status=400, mimetype='application/json')
+    finally:
+        cursor.close()
+
+# Eliminar producto del carrito del usuario
+@app.route('/carrito/producto', methods=['DELETE'])
+def delProductFromShoppingCar():
+    try:
+        cursor = mysql.connection.cursor()
+        iduser = request.json['id_usuario']
+        idproduct = request.json['id_producto']
+
+        query = '''DELETE FROM carrito
+                   WHERE producto_idproducto = %s AND usuario_idusuario = %s'''
+        values = (int(idproduct), int(iduser))
+
+        cursor.execute(query, values)
+        cursor.connection.commit()
+        return Response('{"msg":"Producto eliminado del carrito."}', status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"No se pudo eliminar el producto del carrito."}', status=400, mimetype='application/json')
+    finally:
+        cursor.close()
+
+# Eliminar producto del carrito del usuario
+@app.route('/carrito', methods=['PATCH'])
+def updateProductQuantity():
+    try:
+        cursor = mysql.connection.cursor()
+        iduser = request.json['id_usuario']
+        idproduct = request.json['id_producto']
+        quantity = request.json['cantidad']
+
+        query = '''UPDATE carrito
+                   SET cantidad = %s
+                   WHERE producto_idproducto = %s AND usuario_idusuario = %s;'''
+        values = (int(quantity), int(idproduct), int(iduser))
+
+        cursor.execute(query, values)
+        cursor.connection.commit()
+        return Response('{"msg":"Producto actualizado en el carrito."}', status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"No se pudo actualizar el producto del carrito."}', status=400, mimetype='application/json')
+    finally:
+        cursor.close()
+
+# Valida el inicio de sesión de un usuario
+@app.route('/usuario/sesion', methods=['POST'])
+def userLogin():
+    try:
+        cursor = mysql.connection.cursor()
+        email = request.json['email']
+        password = request.json['password']
+
+        query = '''SELECT * FROM usuario WHERE email = %s AND contrasena = %s'''
+        values = (email,password)
+
+        cursor.execute(query, values)
+        result = cursor.fetchall()
+
+        resjson = json.dumps(result[0], cls = Encoder)
+        print(resjson)
+        return Response('{"msg":"Usuario válido."}', status=200, mimetype='application/json')
+    except Exception as e:
+        print('[ERROR]:',e)
+        return Response('{"msg":"Usuario no autorizado."}', status=401, mimetype='application/json')
     finally:
         cursor.close()
 
